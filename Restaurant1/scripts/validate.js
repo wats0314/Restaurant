9db7d5895d6ce2b31461e4b8b4f808ffec38d0b6
Mitch Watson <Wats0314@d.umn.edu>
2020-01-10 23:03:50 -0600

    validationJS

--- Restaurant1/scripts/validate.js
+++ Restaurant1/scripts/validate.js
@@ -1,4 +1,4 @@
-function clearForm(){
+function resetForm(){
 		document.getElementById("name").value = "";
 		document.getElementById("email").value = "";
 		document.getElementById("phone").value = "";
@@ -10,8 +10,44 @@ function clearForm(){
 		document.getElementById("checkbox4").checked = false;
 		document.getElementById("checkbox5").checked = false;
 		document.getElementById("noBox").checked = true;
-		return false;
 }
 
+function clearErrors(){
+	document.getElementById("name").className = "form-control";
+	document.getElementById("email").className = "form-control";
+	document.getElementById("phone").className = "form-control";
+}
+	
 
-//function validate()
+
+function validate(){
+	
+	clearErrors();
+	
+	
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var phone = document.getElementById("phone").value;
+		
+	
+	if (name == ""){alert("You must enter your name!");
+		document.getElementById("name").className = "form-control is-invalid";
+	return false;}
+	else {document.getElementById("name").className = "form-control is-valid"}
+	
+	
+	if (email == ""){alert("You must enter an Email!");
+			document.getElementById("email").className = "form-control is-invalid";
+	return false;}
+	else {document.getElementById("email").className = "form-control is-valid"}
+
+
+	if (phone == ""){alert("You must enter a phone number!");
+	document.getElementById("phone").className = "form-control is-invalid";
+	return false;}
+	else {document.getElementById("phone").className = "form-control is-valid";
+	alert("Thank you for your submission! We will be in contact with you soon!")}
+	
+	return false
+	}
+	
