46ee1ab8712150256b55e59e16bcd98005fe95b1
Mitch Watson <Wats0314@d.umn.edu>
2020-01-10 21:30:58 -0600

    clearformsadded

---
+++ Restaurant1/scripts/validate.js
@@ -0,0 +1,17 @@
+function clearForm(){
+		document.getElementById("name").value = "";
+		document.getElementById("email").value = "";
+		document.getElementById("phone").value = "";
+		document.getElementById("contactReason").value = "default";
+		document.getElementById("addInfo").value = "";
+		document.getElementById("checkbox1").checked = false;
+		document.getElementById("checkbox2").checked = false;
+		document.getElementById("checkbox3").checked = false;
+		document.getElementById("checkbox4").checked = false;
+		document.getElementById("checkbox5").checked = false;
+		document.getElementById("noBox").checked = true;
+		return false;
+}
+
+
+//function validate()
